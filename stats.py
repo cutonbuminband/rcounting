4ae32428f9c12d36e0278b919de805a7a29bb86f
CutOnBumInBandHere <cutonbuminband@gmail.com>
2021-05-13 18:05:48 +0200

    Hoist get statistics out of the loop
    After refactoring the code so that the target comment for the routine is the
    get, we no longer need to keep track of last and second last comments inside the
    loop over the thread - we can just ask who made the last comment and what was
    the timestamp right away. That lets us further simplify the loop logic.

--- stats.py
+++ stats.py
@@ -3,8 +3,6 @@ import datetime
 import time
 import configparser
 import requests.auth
-from urllib.parse import urlunparse
-from pathlib import PurePath
 from collections import OrderedDict, defaultdict
 
 t_start = datetime.datetime.now()
@@ -29,11 +27,10 @@ headers = {"Authorization": "bearer " + access_token,
            "User-Agent": "cobibh/counting_stats/v1"}
 
 all_the_data = []
-temp = True
 id_get = 'e1slbz0'
 id_main = id_get
 id_thread = '8w151j'
-timestamp_noted = False
+baseCount = 2171000
 
 def get_data(json_response):
     return json_response['data']['children'][0]['data']
@@ -46,32 +43,27 @@ def parse_data(comment_data):
                        thread_id=comment_data['link_id'].split("_", 1)[1],
                        parent_id=comment_data['parent_id'].split("_", 1)[1])
 
-response_thread = requests.get("http://oauth.reddit.com/by_id/t3_" + id_thread + ".json",
-                               headers=headers).json()
-title = get_data(response_thread)['title']
-body = get_data(response_thread)['selftext']
 netloc = 'oauth.reddit.com'
-scheme = 'https'
-path = f'/r/counting/comments/{id_thread}/c/' + '{}'
-url = urlunparse([scheme, netloc, path, '', 'context=10', ''])
-failure_url = urlunparse([scheme, netloc, 'api/info.json', '', 'id=t1_{}', ''])
+path = f'r/counting/comments/{id_thread}/c/' + '{}'
+batch_url = f"http://{netloc}/{path}?context=10"
+single_url = f"https://{netloc}/api/info.json?id=t1_" + "{}"
+
 while id_main != id_thread:
     # Request 10 comments in the thread, based on the latest count we have
     # received. The returned data is a nested dict, starting at the 10th parent
     # of the base comment and moving down from there.
-    current_comments = requests.get(url.format(id_main),
+    current_comments = requests.get(batch_url.format(id_main),
                                     headers=headers).json()[1]
     temp_data = []
     data = parse_data(get_data(current_comments))
     next_target = data['parent_id']
-    timestamp_first = data['timestamp']
+    thread_start = data['timestamp']
     id_first_comment = data['comment_id']
     while id_main != next_target:
         if get_data(current_comments)['id'] != '_':
             # The happy path. We parse all the comments we have received, and
             # when we are done we ask for a new batch.
             current_data = parse_data(get_data(current_comments))
-            timestamp = current_data['timestamp']
             if current_data['comment_id'] == id_main:
                 id_main = next_target
             temp_data.append(tuple(current_data.values())[:-1])
@@ -79,34 +71,29 @@ while id_main != id_thread:
         else:
             print("\n\nBroken chain detected\n" + f"Last id_main: {id_main}\n")
             for x in range(25):
-                broken_comment = requests.get(failure_url.format(id_main),
+                broken_comment = requests.get(single_url.format(id_main),
                                               headers=headers).json()
                 broken_data = parse_data(get_data(broken_comment))
 
                 if id_main != '_':
                     all_the_data.append(tuple(broken_data.values())[:-1])
-                if not timestamp_noted:
-                    get_author = broken_data['author']
-                    timestamp_last = broken_data['timestamp']
-                    timestamp_noted = True
                 id_main = broken_data['parent_id']
             print(f"\nNew id_main: {id_main}")
             print("\nEnd of broken chain\n\n")
             temp_data = []
             break
-    if not timestamp_noted:
-        get_author = current_data['author']
-        timestamp_last = current_data['timestamp']
-        timestamp_noted = True
     for l in reversed(temp_data):
         all_the_data.append(l)
 
     print (id_main)
 
-thread_time = datetime.timedelta(seconds=(timestamp_last - timestamp_first))
+last_comment = requests.get(single_url.format(id_get), headers=headers).json()
+last_comment = parse_data(get_data(last_comment))
+thread_end = last_comment['timestamp']
+
+thread_time = datetime.timedelta(seconds=(thread_end - thread_start))
 days = thread_time.days
 hours, mins, secs = str(thread_time).split(':')[-3:]
-baseCount = 2171000
 
 dict_count = defaultdict(int)
 with open("thread_log.csv", "w") as hog_file:
@@ -117,6 +104,11 @@ with open("thread_log.csv", "w") as hog_file:
                 print(baseCount + idx, *x[1:], sep=",", file=hog_file)
             except:
                 continue
+
+response_thread = requests.get(f"https://{netloc}/by_id/t3_{id_thread}.json",
+                               headers=headers).json()
+title = get_data(response_thread)['title']
+body = get_data(response_thread)['selftext']
 
 with open("thread_participation.csv", "w") as hoc_file:
     hoc_file.write("Thread Participation Chart for " + title + "\n\n")
@@ -131,7 +123,7 @@ with open("thread_participation.csv", "w") as hoc_file:
 
     for counter in reversed(sorted_list):
         unique_counters += 1
-        if counter[0][3:] == get_author:
+        if counter[0][3:] == last_comment['author']:
             counter = (f"**{counter[0]}**", counter[1])
         print(unique_counters, *counter, sep="|", file=hoc_file)
 
