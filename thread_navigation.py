5caffc50f6b36d8ac7da3cdac2f1f652a094fc78
CutOnBumInBandHere <cutonbuminband@gmail.com>
2021-05-19 10:35:04 +0200

    Use pushshift to try and recover deleted accounts and comments
    Sometimes people delete their accounts, their comments, and sometimes people are
    shadowbanned and everything shows up as "removed by moderator". Even in these
    cases, we can access the relevant submission and comment id's - there's just no
    data associated with them. Pushshift has an archive of reddit submissions and
    comments, so we query it to see if it has any data on the relevant id's.

--- thread_navigation.py
+++ thread_navigation.py
@@ -1,9 +1,28 @@
 import requests.auth
 from praw.models import MoreComments
+from praw.models import Comment
 from praw.exceptions import ClientException
 from parsing import find_urls_in_text, find_count_in_text
 
 ps_url = 'https://api.pushshift.io/reddit'
+
+
+def get_text(reddit_post):
+    if isinstance(reddit_post, Comment):
+        is_comment = True
+        post_type = 'comment'
+    else:
+        is_comment = False
+        post_type = 'submission'
+    body = reddit_post.body if is_comment else reddit_post.selftext
+    if body == "[deleted]" or body == "[removed]":
+        ps_post = requests.get(f"{ps_url}/{post_type}/search?ids={reddit_post.id}")
+        try:
+            post_dict = ps_post.json()['data'][0]
+            body = post_dict['body'] if is_comment else post_dict['selftext']
+        except (KeyError, IndexError):
+            body = "[deleted]"
+    return body
 
 
 def find_previous_get(get_id, reddit_instance):
@@ -11,7 +30,7 @@ def find_previous_get(get_id, reddit_instance):
     thread = comment.submission
     urls = find_urls_in_text(thread.selftext)
     if not urls:
-        urls = [find_urls_in_text(comment.body) for comment in thread.comments]
+        urls = [find_urls_in_text(get_text(comment)) for comment in thread.comments]
         urls = [url for comment_urls in urls for url in comment_urls]
     new_thread_id, title_url, new_get_id = urls[0]
     if not new_get_id:
@@ -29,7 +48,7 @@ def find_get_in_thread(thread, reddit):
     for comment_id in comment_ids[:-1]:
         comment = reddit.comment(comment_id)
         try:
-            count = find_count_in_text(comment.body)
+            count = find_count_in_text(get_text(comment))
             if count % 1000 == 0:
                 return comment.id
         except ValueError:
@@ -41,7 +60,7 @@ def search_up_from_gz(comment, max_retries=5):
     "Find a count up to max_retries above the linked_comment"
     for i in range(max_retries):
         try:
-            count = find_count_in_text(comment.body)
+            count = find_count_in_text(get_text(comment))
             break
         except ValueError:
             if i == max_retries:
@@ -60,7 +79,7 @@ def find_get_from_comment(comment):
         comment = comment.replies[0]
         if isinstance(comment, MoreComments):
             comment = comment.comments()[0]
-        count = find_count_in_text(comment.body)
+        count = find_count_in_text(get_text(comment))
     return comment
 
 
@@ -80,8 +99,16 @@ def extract_gets_and_assists(comment, reddit_instance, n_threads=1000):
 
 
 def comment_to_tuple(comment):
+    if comment.author is None:
+        try:
+            ps_comment = requests.get("https://api.pushshift.io/reddit/comment/search?"
+                                      f"ids={comment.id}").json()
+            author = ps_comment['data'][0]['author']
+        except (KeyError, IndexError):
+            print(comment.id)
+            author = "[deleted]"
     author = str(comment.author) if comment.author is not None else "[deleted]"
-    return (comment.body, author,
+    return (get_text(comment), author,
             comment.created_utc, comment.id, str(comment.submission))
 
 
