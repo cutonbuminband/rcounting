450454d43d3b1f1373ab1613077f4cc0431c6f28
CutOnBumInBandHere <cutonbuminband@gmail.com>
2021-05-21 14:31:00 +0200

    Add comments for each of the validation rules

--- validation.py
+++ validation.py
@@ -15,27 +15,36 @@ def validate_thread(comment, rule):
         return (False, df[~mask].iloc[0]['comment_id'])
 
 
-def wait_2_rule(df):
-    interval = df.groupby('username').diff()['running_index']
-    return interval.isna() | (interval > 2)
+def wait_2(df):
+    # You can only count after two others have counted
+    skip_counts = df.groupby('username').diff()['running_index']
+    return skip_counts.isna() | (skip_counts > 2)
 
 
-def wait_3_rule(df):
-    interval = df.groupby('username').diff()['running_index']
-    return interval.isna() | (interval > 3)
+def wait_3(df):
+    # You can only count after three others have counted
+    skip_counts = df.groupby('username').diff()['running_index']
+    return skip_counts.isna() | (skip_counts > 3)
 
 
-def slow_rule(df):
-    interval = df.groupby('username').diff()['running_index']
-    valid_interval = interval.isna() | (interval > 1)
+def once_per_thread(df):
+    skip_counts = df.groupby('username').diff()['running_index']
+    return skip_counts.isna()
+
+
+def slow(df):
+    # You cannot reply to yourself and must wait 1 minute since last count
+    skip_counts = df.groupby('username').diff()['running_index']
+    valid_skip = skip_counts.isna() | (skip_counts > 1)
     elapsed_time = df['timestamp'].diff()
     valid_time = elapsed_time.isna() | (elapsed_time >= 60)
-    return valid_interval & valid_time
+    return valid_skip & valid_time
 
 
-def slower_rule(df):
-    interval = df.groupby('username').diff()['running_index']
-    valid_interval = interval.isna() | (interval > 1)
+def slower(df):
+    # You cannot reply to yourself and can only count once per hour
+    skip_counts = df.groupby('username').diff()['running_index']
+    valid_skip = skip_counts.isna() | (skip_counts > 1)
     elapsed_time = df.groupby('username').diff()['timestamp']
     valid_time = elapsed_time.isna() | (elapsed_time >= 60 * 60)
-    return valid_interval & valid_time
+    return valid_skip & valid_time
