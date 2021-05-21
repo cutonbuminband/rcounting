8ffdd311cbda9697c42df9a59b46dbf20228c07d
CutOnBumInBandHere <cutonbuminband@gmail.com>
2021-05-21 14:30:43 +0200

    Add a docstring explaining how the fft kde works

--- analysis.py
+++ analysis.py
@@ -44,16 +44,34 @@ def vonmises_distribution(x, mu, kappa):
     return np.exp(kappa * np.cos(x - mu)) / (2. * np.pi * i0(kappa))
 
 
-def vonmises_fft_kde(data, kappa, n_bins):
-    """ Return the kde of data using the von mises distribution on the circle
+def fft_kde(data, n_bins, kernel=vonmises_distribution, **params):
+    """The fft approximation to the kde of data on the unit circle
+
+    params:
+    data: array-like: The data points to be approximated. Assumed to be distributed on [-pi, pi]
+    n_bins: number of bins to use for approximation
+    kernel: The kernel used to approximate the data. Default is the gaussian on the circle
+    **params: parameters apart from x needed to define the kernel.
+
+    returns:
+    x: array-like: points on the unit circle at which to evaluate the density
+    kde: Estimated density, normalised so that ∫kde dx = 1.
+
+    In genergal, kernel density estimation means replacing each discrete point
+    with some normalised distribution centered at that point, and then adding
+    up all these results. Mathematically, this corresponds to convolving the
+    original data with the chosen distribution. In fourier space, a convolution
+    transforms to a product, so for periodic data we can get a fast
+    approximation to this by finding the Fourier transform of our data, the
+    Fourier transform of our kernel, multiplying them together and finding the
+    inverse transform.
+
     """
 
-    bins = np.linspace(-np.pi, np.pi, n_bins + 1, endpoint=True)
-    hist_n, bin_edges = np.histogram(data, bins=bins)
-    bin_centers = np.mean([bin_edges[1:], bin_edges[:-1]], axis=0)
-    kernel = vonmises_distribution(x=bin_centers,
-                                   mu=0,
-                                   kappa=kappa)
-    kde = fftshift(irfft(rfft(kernel) * rfft(hist_n)))
-    kde /= np.trapz(kde, x=bin_centers)
-    return bin_centers, kde
+    x_axis = np.linspace(-np.pi, np.pi, n_bins + 1, endpoint=True)
+    hist, edges = np.histogram(data, bins=x_axis)
+    x = np.mean([edges[1:], edges[:-1]], axis=0)
+    kernel = kernel(x=x, **params)
+    kde = fftshift(irfft(rfft(kernel) * rfft(hist)))
+    kde /= np.trapz(kde, x=x)
+    return x, kde
