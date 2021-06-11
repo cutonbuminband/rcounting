f25a9b662df7a3cf6d75d53d9cd9d08ab97df904
CutOnBumInBandHere <cutonbuminband@gmail.com>
2021-06-11 16:38:50 +0200

    Add function to calculate user adjacency list

--- analysis.py
+++ analysis.py
@@ -36,6 +36,16 @@ def is_mod(username):
     return username in mods
 
 
+def response_graphf(df, n=250):
+    undeleted_users = df['username'] != '[deleted]'
+    user_counts = df.loc[undeleted_users].groupby('username')['timestamp'].count()
+    top = user_counts.sort_values().tail(n).index
+    edges = df.username.isin(top) & df.username.shift(-1).isin(top)
+    df['replying_to'] = df.username.shift(-1)
+    graph = df.loc[edges].groupby(['username', 'replying_to'], as_index=False)['timestamp'].count()
+    return graph
+
+
 def capture_the_flag_score(thread):
     thread['score'] = thread['timestamp'].diff().shift(-1)
     return thread.groupby('username')['score'].sum()
