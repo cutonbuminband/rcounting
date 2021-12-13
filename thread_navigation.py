cutonbuminband/rcounting

commit 0836cc185196cc04a999293e69688f30d8e2eec9

Rename `fetch_thread` -> `fetch_comments`

thread_navigation.py
@@ -96,7 +96,6 @@ def fetch_comment_tree(submission, root_id=None, verbose=True, use_pushshift=Tru
     return submission_tree
 
 
-def fetch_thread(comment, verbose=True):
-    tree = fetch_comment_tree(comment.submission, verbose=verbose)
+def fetch_comments(comment, verbose=True):
     comments = tree.comment(comment.id).walk_up_tree()[::-1]
     return [x.to_dict() for x in comments]

db.py
@@ -39,7 +39,7 @@ def table_to_submission_id(x):
 
 
 if __name__ == "__main__":
-    from thread_navigation import fetch_thread
+    from thread_navigation import fetch_comments
     from reddit_interface import reddit
 
     df = pd.read_csv(Path('~/Downloads/ALL_clean.csv'), usecols=[1, 2, 3, 4, 5])
@@ -59,7 +59,7 @@ if __name__ == "__main__":
             continue
         print(f"Writing submission {submission} to db")
         comment = reddit.comment(submission_df.iloc[-1]['comment_id'])
-        comments = fetch_thread(comment)
+        comments = fetch_comments(comment)
 
         new_df = pd.DataFrame(comments)
         merged_df = pd.merge(submission_df.drop(['timestamp', 'submission_id'], axis=1),

log_thread.py
@@ -2,7 +2,7 @@
 import os
 from pathlib import Path
 from parsing import post_to_count
-from thread_navigation import fetch_thread, find_previous_get
+from thread_navigation import fetch_comments, find_previous_get
 import pandas as pd
 from aliases import apply_alias
 from utils import format_timedelta
@@ -15,7 +15,7 @@ def log_one_submission(leaf_comment):
     if os.path.isfile(hoc_path):
         return
 
-    comments = fetch_thread(leaf_comment)
+    comments = fetch_comments(leaf_comment)
 
     title = leaf_comment.submission.title
     df = pd.DataFrame(comments)

validate.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     import pandas as pd
     from side_threads import get_side_thread
     from reddit_interface import reddit
-    from thread_navigation import fetch_thread
+    from thread_navigation import fetch_comments
     import argparse
 
     rule_dict = {'default': 'default',
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                  'slowestest': 'slowestest',
                  'only_double_counting': 'only_double_counting'}
 
-    parser = argparse.ArgumentParser(description='Validate the reddit thread which'
+    parser = argparse.ArgumentParser(description='Validate the reddit submission which'
                                      ' contains the comment with id `comment_id` according to rule')
     parser.add_argument('comment_id',
                         help='The id of the comment to start logging from')
@@ -25,10 +25,10 @@ if __name__ == "__main__":
                         help='Which rule to apply. Default is no double counting')
     args = parser.parse_args()
 
-    comments = fetch_thread(reddit.comment(args.comment_id))
-    thread = pd.DataFrame(comments)
+    comments = fetch_comments(reddit.comment(args.comment_id))
+    comments = pd.DataFrame(comments)
     side_thread = get_side_thread(rule_dict[args.rule])
-    result = side_thread.is_valid_thread(thread)
+    result = side_thread.is_valid_thread(comments)
     if result[0]:
         print('All counts were valid')
     else:

commit 8fb61c42a941d660f88c400e81150646d4181fdd

Fix error in thread navigation
0836cc1 accidentally deleted a line in fetch_thread while refactoring. It's
already pushed to github, so the fix has to be done through a new commit instead
of amending the existing one.

thread_navigation.py
@@ -97,5 +97,6 @@ def fetch_comment_tree(submission, root_id=None, verbose=True, use_pushshift=Tru
 
 
 def fetch_comments(comment, verbose=True):
+    tree = fetch_comment_tree(comment.submission, verbose=verbose)
     comments = tree.comment(comment.id).walk_up_tree()[::-1]
     return [x.to_dict() for x in comments]