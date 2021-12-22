2d70cf3ec1558ebe59867815ef8b8abb8e758835
CutOnBumInBandHere <cutonbuminband@gmail.com>
2021-12-22 14:48:29 +0100

    Use tokens for authorization rather than usernames and passwords
    Reddit has different types of apps, including `script` and `installed`.
    
    - Script apps are assumed to run on machines under the control of the developer,
      are able to hold a client secret, and typically authenticate using usernames
      and passwords. They typically interact only with the developer's account, or
      with accounts directly controlled by the developer.
    - Installed apps run on machines the developer doesn't control, for and by other
      users. RIF is an example of this kind. For these apps, authentication and
      refresh tokens should be used instead of passwords.
    
    The advantage of using this flow is that anybody who tries to download the tools
    here (eg through) pip doesn't need to ceate their own app, and doesn't need to
    trust this program with the entirety of their account, but only with read,
    wikiread and wikiedit privileges. Read and wikiread are basically free, since
    anybody could do that from a web browser anyway (apart from for private
    subreddits).

--- rcounting/reddit_interface.py
+++ rcounting/reddit_interface.py
@@ -1,30 +1,96 @@
+import os
+import socket
+import random
+import configparser
 import praw
 
-# You need [OAuth](https://github.com/reddit-archive/reddit/wiki/OAuth2)
-# credentials to interact with reddit, and you need to tell praw where to find
-# them. This can be done in one of three ways: either create a file called
-# praw.ini in the current directory with the following contents:
+# The script works with OAuth access and refresh tokens, so you need to grant
+# access. Once you've done that, they token will be stored in a file for future
+# use.
+_client_id = "S2MLNKwl9tpgaS5jUtlIIQ"
+_user_agent = "rcounting_tools/v0.3 by u/CutOnBumInBandHere"
 
-# [your_bot_name]
-# client_id = 14CHARACTER_ID
-# client_secret = 30CHARACTER_SECRET
-# user_agent= PICK_SOMETHING_SENSIBLE
-# username = USERNAME
-# password = PASSWORD
 
-# and then uncomment this call
-# reddit = praw.Reddit('your_bot_name')
-#
+def get_refresh_token():
+    # To do logging, reading reddit posts is necessary
+    # To update the wiki, both wikiread and wikiedit are needed
+    scopes = ['read', 'wikiread', 'wikiedit']
 
-# Alternatively, you can write the credentials directly into this file, and
-# make the call
-# reddit = praw.Reddit(client_id="14CHARACTER_ID",
-#                      client_secret="30CHARACTER_SECRET",
-#                      user_agent="PICK_SOMETHING_SENSIBLE",
-#                      username="USERNAME",
-#                      password="PASSWORD")
+    reddit = praw.Reddit(
+        client_id=_client_id,
+        user_agent=_user_agent,
+        redirect_uri="http://localhost:8080",
+        client_secret=None,
+    )
+    print("This is the first time you're using the rcounting tools.",
+          "You need to allow the program to interact with reddit on your behalf",
+          "For logging, it needs to be able to read wiki pages and posts, "
+          "and to update the thread directory it needs permission to edit wiki pages.",
+          sep="\n")
+    state = str(random.randint(0, 65000))
+    url = reddit.auth.url(scopes, state, "permanent")
+    print(f"Please open this url in a web browser, and follow the instructions there: {url}")
 
-# Finally, praw looks for environment variables called "praw_client_id" etc,
-# and if you've set those, you can just write
+    client = receive_connection()
+    data = client.recv(1024).decode("utf-8")
+    param_tokens = data.split(" ", 2)[1].split("?", 1)[1].split("&")
+    params = {
+        key: value for (key, value) in [token.split("=") for token in param_tokens]
+    }
 
-reddit = praw.Reddit(user_agent="cobibh_counting_bot/v0.2")
+    if state != params["state"]:
+        send_message(
+            client,
+            f"State mismatch. Expected: {state} Received: {params['state']}",
+        )
+        return 1
+    elif "error" in params:
+        send_message(client, params["error"])
+        return 1
+
+    refresh_token = reddit.auth.authorize(params["code"])
+    send_message(client,
+                 ("Successfully retrieved refresh token! "
+                  "You can close this window now"))
+    return refresh_token
+
+
+def receive_connection():
+    """Wait for and then return a connected socket..
+
+    Opens a TCP connection on port 8080, and waits for a single client.
+
+    """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("localhost", 8080))
+    server.listen(1)
+    client = server.accept()[0]
+    server.close()
+    return client
+
+
+def send_message(client, message):
+    """Send message to client and close the connection."""
+    print(message)
+    client.send(f"HTTP/1.1 200 OK\r\n\r\n{message}".encode("utf-8"))
+    client.close()
+
+
+refresh_token = os.getenv('praw_refresh_token')
+if refresh_token is None:
+    module_dir = os.path.dirname(__file__)
+    credentials_file = os.path.join(module_dir, "credentials.ini")
+    if not os.path.isfile(credentials_file):
+        refresh_token = get_refresh_token()
+        with open(credentials_file, 'w') as f:
+            print(f"[tokens]\nrefresh_token = {refresh_token}",
+                  file=f)
+    config = configparser.ConfigParser()
+    config.read(credentials_file)
+    refresh_token = config["tokens"]["refresh_token"]
+
+reddit = praw.Reddit(client_id=_client_id,
+                     user_agent=_user_agent,
+                     client_secret=None,
+                     refresh_token=refresh_token)
