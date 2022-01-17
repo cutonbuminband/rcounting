23492508f16f837602f92beb1870488bd74909f8
CutOnBumInBandHere <cutonbuminband@gmail.com>
2022-01-17 07:21:03 +0100

    Move threadlogger to its own file

--- rcounting/scripts/log_thread.py
+++ rcounting/scripts/log_thread.py
@@ -1,18 +1,16 @@
 """Script for logging reddit submissions to either a database or a csv file"""
-import functools
 import logging
-import os
-import sqlite3
 from datetime import datetime
 from pathlib import Path
 
 import click
 import pandas as pd
 
-from rcounting import configure_logging, models, parsing
+from rcounting import configure_logging
 from rcounting import thread_directory as td
 from rcounting import thread_navigation as tn
 from rcounting import utils
+from rcounting.io import ThreadLogger
 from rcounting.reddit_interface import reddit, subreddit
 
 printer = logging.getLogger("rcounting")
@@ -27,72 +25,6 @@ def find_comment(leaf_comment_id, threads):
         comment_id = threads[0][4]
         return tn.find_previous_get(reddit.comment(comment_id))
     return reddit.comment(leaf_comment_id)
-
-
-class ThreadLogger:
-    """Simple class for logging either to a database or to a csv file"""
-
-    def __init__(self, sql, output_directory, filename=None):
-        self.sql = sql
-        self.output_directory = output_directory
-        self.last_checkpoint = ""
-        if self.sql:
-            self.setup_sql(filename)
-        self.log = self.log_sql if self.sql else self.log_csv
-
-    def setup_sql(self, filename):
-        """Connect to the database and get list of existing submissions, if any"""
-        last_checkpoint = ""
-        known_submissions = []
-        if filename is None:
-            filename = "counting.sqlite"
-        path = self.output_directory / filename
-        printer.warning("Writing submissions to sql database at %s", path)
-        db = sqlite3.connect(path)
-        try:
-            known_submissions = pd.read_sql("select * from submissions", db)[
-                "submission_id"
-            ].tolist()
-            checkpoints = pd.read_sql("select submission_id from last_submission", db)
-            last_checkpoint = checkpoints.iat[-1, 0]
-        except pd.io.sql.DatabaseError:
-            pass
-        self.db = db
-        self.last_checkpoint = last_checkpoint
-        self.known_submissions = known_submissions
-
-    def is_already_logged(self, comment):
-        """Determine whether a submission has already been logged"""
-        if self.sql:
-            return comment.submission.id in self.known_submissions
-        body = parsing.strip_markdown_links(comment.body)
-        basecount = parsing.find_count_in_text(body) - 1000
-        hoc_path = self.output_directory / Path(f"{basecount}.csv")
-        return os.path.isfile(hoc_path)
-
-    def log_sql(self, comment):
-        """Save one submission to a database"""
-        df = pd.DataFrame(tn.fetch_comments(comment))
-        submission = pd.Series(models.Submission(comment.submission).to_dict())
-        submission = submission[["submission_id", "username", "timestamp", "title", "body"]]
-        submission["integer_id"] = int(submission["submission_id"], 36)
-        df.to_sql("comments", self.db, index_label="position", if_exists="append")
-        submission.to_frame().T.to_sql("submissions", self.db, index=False, if_exists="append")
-
-    def log_csv(self, comment):
-        """Save one submission to a csv file"""
-        df = pd.DataFrame(tn.fetch_comments(comment))
-        extract_count = functools.partial(parsing.find_count_in_text, raise_exceptions=False)
-        n = int(1000 * ((df["body"].apply(extract_count) - df.index).median() // 1000))
-        path = self.output_directory / Path(f"{n}.csv")
-
-        columns = ["username", "timestamp", "comment_id", "submission_id"]
-        output_df = df.set_index(df.index + n)[columns].iloc[1:]
-        printer.debug("Writing submission log to %s", path)
-        header = ["username", "timestamp", "comment_id", "submission_id"]
-        with open(path, "w", encoding="utf8") as f:
-            print(f"# {comment.submission.title}", file=f)
-            print(output_df.to_csv(index_label="count", header=header), file=f, end="")
 
 
 @click.command()
@@ -179,8 +111,9 @@ def log(
     ):
         printer.info("Logging %s", comment.submission.title)
         completed += 1
+        df = pd.DataFrame(tn.fetch_comments(comment))
         if not threadlogger.is_already_logged(comment):
-            threadlogger.log(comment)
+            threadlogger.log(comment, df)
         else:
             printer.info("Submission %s has already been logged!", comment.submission.title)
 
