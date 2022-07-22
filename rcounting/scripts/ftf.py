db98f00a24fd0677b28eb9c5a7f34916d44e228b
CutOnBumInBandHere <cutonbuminband@gmail.com>
2022-07-22 16:13:26 +0200

    Add script to take care of the weekly FTF
    Every week there's a free talk friday thread.
    
    The first thread posted after 7am utc on friday is valid. A valid ftf must be
    stickied and have its suggested sort set to new. This script
    
    1) Checks if the stickied ftf is the one from this week
    2) If it's not, it looks to see if a valid ftf has been posted; if not, it posts
    one itself
    3) It then distinguishes the submission and sets the suggested sort

---
+++ rcounting/scripts/ftf.py
@@ -0,0 +1,56 @@
+import datetime as dt
+
+from rcounting.reddit_interface import subreddit
+
+threshold_timestamp = dt.datetime.combine(dt.date.today(), dt.time(hour=7)).timestamp()
+
+
+def is_within_threshold(post):
+    return post.created_utc > threshold_timestamp
+
+
+def find_manual_ftf():
+    submissions = []
+    for submission in subreddit.new(limit=1000):
+        if submission.created_utc >= threshold_timestamp:
+            submissions.append(submission)
+        else:
+            break
+    candidate_ftfs = [
+        submission for submission in submissions if "Free Talk Friday" in submission.title
+    ]
+    return candidate_ftfs[-1] if candidate_ftfs else False
+
+
+def generate_new_title(previous_title):
+    n = int(previous_title.split("#")[1])
+    return f"Free Talk Friday #{n+1}"
+
+
+def generate_new_body(previous_ftf_id):
+
+    ftf_body = (
+        "Continued from last week's FTF [here](/comments/{}/)\n\n"
+        "It's that time of the week again. Speak anything on your mind! "
+        "This thread is for talking about anything off-topic, "
+        "be it your lives, your strava, your plans, your hobbies, studies, stats, "
+        "pets, bears, hikes, dragons, trousers, travels, transit, cycling, family, "
+        "or anything you like or dislike, except politics\n\n"
+        "Feel free to check out our [tidbits](https://redd.it/n6onl8) thread "
+        "and introduce yourself if you haven't already."
+    )
+
+    return ftf_body.format(previous_ftf_id)
+
+
+previous_ftf_post = subreddit.sticky(number=2)
+
+if not is_within_threshold(previous_ftf_post):
+    ftf_post = find_manual_ftf()
+    if not ftf_post:
+        title = generate_new_title(previous_ftf_post.title)
+        body = generate_new_body(previous_ftf_post.id)
+        ftf_post = subreddit.submit(title=title, selftext=body)
+
+    ftf_post.mod.distinguish()
+    ftf_post.mod.suggested_sort(sort="new")
