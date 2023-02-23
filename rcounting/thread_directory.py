8e5948ca489d4d793147241b8ecfc2bb0b6d968e
CutOnBumInBandHere <cutonbuminband@gmail.com>
2023-02-23 07:25:06 +0100

    Ignore DuplicateReplace errors for now an soldier on
    Reddit has started sending commens in a way that praw is not expecting for
    counts that occurred around 21st February 2023. For now there's no way to
    recover from this, so the best approach is to abort the update of the specific
    row, log a message and move on.
    
    It seems that moving up a chain still works, so if tthis persists I might have
    to start looking at that as a workaround.

--- rcounting/thread_directory.py
+++ rcounting/thread_directory.py
@@ -4,6 +4,8 @@ import datetime
 import itertools
 import logging
 
+from praw.exceptions import DuplicateReplaceException
+
 from rcounting import models, parsing
 from rcounting import side_threads as st
 from rcounting import utils
@@ -237,6 +239,10 @@ class Paragraph:
             try:
                 row.update(tree)
                 result.add(row.submission_id)
+            except DuplicateReplaceException as e:
+                printer.warning(
+                    "Thread %s raised DuplicateReplaceException and was not updated.", row.title
+                )
             except Exception:  # pylint: disable=broad-except
                 printer.warning("Unable to update thread %s", row.title)
                 raise
