a0313088bc23f028b963fbc0441daa12d284b9cf
CutOnBumInBandHere <cutonbuminband@gmail.com>
2025-10-24 17:36:29 +0200

    Add a column to the database tracking the leaf submission
    We do this for each thread in the directory, so that we can tell at a glance
    whether the current submission in the directory is the same as it used to be. If
    it is, then there are no new submissions to log, so there's no point in wasting
    API requests on finding that out.

--- rcounting/scripts/log_all_side_threads.py
+++ rcounting/scripts/log_all_side_threads.py
@@ -11,6 +11,10 @@ printer = logging.getLogger("rcounting")
 
 
 def log_side_threads(filename, verbose, quiet, archive=False):
+    import sqlite3
+
+    import pandas as pd
+
     from rcounting import configure_logging
     from rcounting import thread_directory as td
     from rcounting import thread_navigation as tn
@@ -20,18 +24,45 @@ def log_side_threads(filename, verbose, quiet, archive=False):
     if not archive:
         directory = td.load_wiki_page(subreddit, "directory")
         rows = directory.rows[1:]
+
     else:
         directory = td.load_wiki_page(subreddit, "directory/archive")
         rows = [row for row in directory.rows if row.first_submission in known_thread_ids]
+    db = sqlite3.connect(filename)
+    try:
+        threads = pd.read_sql("select * from threads", db, index_col="thread_id")
+    except DatabaseError:
+        threads = pd.DataFrame([], columns=["thread_name", "leaf_submission_id"])
+        threads.index.name = "thread_id"
+
+    if "leaf_submission_id" not in threads.columns:
+        threads["leaf_submission_id"] = None
     for row in rows:
-        if row.first_submission == row.initial_submission_id:
-            continue
         side_thread_id = row.first_submission
         try:
             side_thread_name = known_thread_ids[side_thread_id]
         except KeyError:
             side_thread_name = f"Unknown side thread: {side_thread_id}"
         submission_id = row.initial_submission_id
+        if side_thread_id not in threads.index:
+            new_row = pd.DataFrame(
+                [
+                    {
+                        "thread_id": side_thread_id,
+                        "thread_name": side_thread_name,
+                        "leaf_submission_id": None,
+                    }
+                ]
+            ).set_index("thread_id")
+            threads = pd.concat([threads, new_row])
+        # If this is the first submission in the thread, or the already-known
+        # leaf submission then there are no new submissions to log, so we just
+        # continue to the next row in the directory
+        if (
+            side_thread_id == submission_id
+            or submission_id == threads.loc[side_thread_id, "leaf_submission_id"]
+        ):
+            continue
         previous_submission, previous_get = tn.find_previous_submission(submission_id)
         if previous_submission is None:
             continue
@@ -52,6 +83,8 @@ def log_side_threads(filename, verbose, quiet, archive=False):
             print_timing=False,
             first_submissions=directory.first_submissions,
         )
+        threads.loc[side_thread_id, "leaf_submission_id"] = submission_id
+        threads.to_sql("threads", db, if_exists="replace")
 
 
 @click.command(name="log-side-threads")
